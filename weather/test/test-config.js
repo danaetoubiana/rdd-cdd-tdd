f14fd57b5505846dce99bb5bddf3e1f87239930e
Danaé Toubiana <danae.toubiana@devinci.fr>
2016-02-23 16:02:19 +0100

    testconfig

--- weather/test/test-config.js
+++ weather/test/test-config.js
@@ -1,11 +1,58 @@
-var assert = require('assert'),
+/*var assert = require('assert'),
     config = require('./../lib/config');
+*/
 
 
-describe('Config', function () {
+    var expect = require('chai').expect;
+    var config = require('./../lib/config');
+
+
+    var units = {
+        type: 'si',
+        tmp: '˚C',
+        speed: 'mps'
+    };
+
+    var ip = '';
+
+
+    describe('Config', function () {
+
+        it('has 3 props', function () {
+            var Config = new config(units, ip);
+            expect(Config.args).to.exist;
+            expect(Config.ip).to.exist;
+            expect(Config.units).to.exist;
+        });
+
+        it('after construction, 2 props are populated', function () {
+            var Config = new config(units, ip);
+            expect(Config.args).to.not.equal(null);
+            expect(Config.units).to.not.equal(null);
+
+        });
+
+        it('has correct key types',function(){
+
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*describe('Config', function () {
     it('has 3 props')
 
     it('after construction, 2 props are populated')
 
     it('has correct key types')
-});
+});*/
